79ab86032496a504a139aa175b1e503cd1956c39
federico.stange@bue.edu.ar <jpfstange@gmail.com>
2017-03-01 02:15:19 -0300

    Added a method to the profile entity factory which allows you to load fields from an ini configuration file, this will permit the end user to edit said configuration file in order to gather less or more information, while running the process through cron or through plain command line

--- src/entity/profile/Factory.class.php
+++ src/entity/profile/Factory.class.php
@@ -38,12 +38,112 @@
 
 			private	$request		=	NULL;
 			private	$objectId	=	NULL;
-			private	$fields		=	Array();
 
-			public function __construct(Request $request,$objectId){
+			//Provide some default fields for each different profile type
+
+			private	$fields		=	[
+												"user"	=>	[
+																'id','cover','birthday','about','link','likes','name',
+																'website','hometown','gender',
+																'interested_in','is_verified','labels','languages',
+																'last_name','first_name','location',
+																'meeting_for','political','public_key','religion',
+																'significant_other','sports','work','accounts',
+																'achievements','groups','movies','music','picture','television',
+																'books','favorite_teams'
+												],
+												'group'	=>	[
+																'id','cover','description','email','icon','link','member_request_count',
+																'name','owner','parent','privacy','purpose','updated_time','venue'
+												],
+												'page'	=>	[
+																'id','about','affiliation','app_id','app_links','artists_we_like',
+																'attire','awards','band_interests','band_members','best_page',
+																'bio','birthday','booking_agent','built','business','can_checkin',
+																'category','category_list','checkins','company_overview','contact_address',
+																'cover','culinary_team','current_location','description','directed_by',
+																'display_subtext','displayed_message_response_time','emails',
+																'fan_count','featured_video','features','food_styles','founded',
+																'general_info','general_manager','genre','global_brand_page_name',
+																'global_brand_root_id','has_added_app','hometown','hours',
+																'impressum','is_always_open','is_community_page','is_permanently_closed',
+																'is_published','is_unclaimed','is_verified','is_webhooks_subscribed',
+																'link','location','members','influences','mission','mpg','name',
+																'name_with_location_descriptor','network','overall_star_rating',
+																'parking','parent_page','personal_info','personal_interests',
+																'pharma_safety_info','phone','place_type','plot_outline',
+																'press_contact','price_range','produced_by','products',
+																'public_transit','publisher_space','rating_count',
+																'record_label','release_date','restaurant_services',
+																'restaurant_specialties','schedule','screenplay_by',
+																'season','single_line_address','starring','start_info',
+																'store_location_descriptor','store_number','studio',
+																'talking_about_count','username','verification_status',
+																'voip_info','website','were_here_count','written_by','likes'
+											],
+											'app'			=>	[],
+											'event'		=>	[]
+			];
+
+			public function __construct(Request $request,$objectId,Array $fields=Array()){
 
 				$this->request		=	$request;
 				$this->objectId	=	$objectId;
+
+				if($fields){
+	
+					$this->setFields($fields);
+
+				}
+
+			}
+
+			public function setFields(Array $fields){
+
+				foreach($fields as $profileType=>$fields){
+
+					if(isset($this->fields[$entityName])){
+
+						$this->fields[$profileType]	=	explode(',',$fields);
+
+					}
+
+				}
+
+				return $this;
+
+			}
+
+			public function getFields(){
+
+				return $this->fields;
+
+			}
+
+			public static function fromIniFile($file,Request $request){
+
+				if(!file_exists($file)){
+
+					throw new \InvalidArgumentException("File \"$file\" does not exists");
+
+				}
+
+				$config	=	parse_ini_file($file,$sections=TRUE);
+
+				if(!$config){
+
+					throw new \RuntimeException("Could not parse ini file \"$file\"");	
+
+				}
+
+				if(!isset($file['profile_fields'])){
+
+					$msg	=	"Could not find section named profile_fields in ini file \"$file\"";
+					throw new \LogicException($msg);
+
+				}
+
+				return new static($request,$objectId,$file['profile_fields']);
 
 			}
 
@@ -61,92 +161,17 @@
 
 			public function build(Array $fields=Array()){
 
-				$type	=	$this->getObjectType();
+				$type				=	$this->getObjectType();
+				$entityClass	=	sprintf("\\stange\\fbsucker\\entity\\profile\\%s",ucwords($type));
 
-				switch($type){
+				if(!class_exists($entityClass)){
 
-					case 'user':
-
-						$default	=	[
-											'id','cover','birthday','about','link','likes','name','website','hometown','gender',
-											'interested_in','is_verified','labels','languages','last_name','first_name','location',
-											'meeting_for','political','public_key','religion','significant_other','sports',
-											'work','accounts','achievements','groups','movies','music','picture','television',
-											'books','favorite_teams'
-						];
-
-						$entity	=	new UserEntity();
-
-					break;
-
-					case 'group':
-	
-						$default	=	[
-											'id','cover','description','email','icon','link','member_request_count',
-											'name','owner','parent','privacy','purpose','updated_time','venue'
-						];
-
-						$entity	=	new GroupEntity();
-
-					break;
-
-					case 'page':
-
-						$default	=	[
-											'id','about','affiliation','app_id','app_links','artists_we_like',
-											'attire','awards','band_interests','band_members','best_page',
-											'bio','birthday','booking_agent','built','business','can_checkin',
-											'category','category_list','checkins','company_overview','contact_address',
-											'cover','culinary_team','current_location','description','directed_by',
-											'display_subtext','displayed_message_response_time','emails',
-											'fan_count','featured_video','features','food_styles','founded',
-											'general_info','general_manager','genre','global_brand_page_name',
-											'global_brand_root_id','has_added_app','hometown','hours',
-											'impressum','is_always_open','is_community_page','is_permanently_closed',
-											'is_published','is_unclaimed','is_verified','is_webhooks_subscribed',
-											'link','location','members','influences','mission','mpg','name',
-											'name_with_location_descriptor','network','overall_star_rating',
-											'parking','parent_page','personal_info','personal_interests',
-											'pharma_safety_info','phone','place_type','plot_outline',
-											'press_contact','price_range','produced_by','products',
-											'public_transit','publisher_space','rating_count',
-											'record_label','release_date','restaurant_services',
-											'restaurant_specialties','schedule','screenplay_by',
-											'season','single_line_address','starring','start_info',
-											'store_location_descriptor','store_number','studio',
-											'talking_about_count','username','verification_status','voip_info','website',
-											'were_here_count','written_by','likes'
-						];
-
-						$entity	=	new PageEntity();
-
-					break;
-
-					case 'application':
-
-						$default	=	[
-						];
-
-						$entity	=	new AppEntity();
-
-					break;
-
-					case 'event':
-
-						$default	=	[
-						];
-
-						$entity	=	new EventEntity();
-
-					break;
-
-					default:
-						throw new \InvalidArgumentException("Unknown object type \"$type\"");
-					break;
+					throw new \InvalidArgumentException("Unknown object type \"$type\"");
 
 				}
 
-				$fields	=	$fields	?	$fields	:	$default;
+				$entity	=	new $entityClass();
+				$fields	=	$fields	?	$fields	:	$this->fields[$type];
 
 				$entity->setGraphData(
 											$this->request
